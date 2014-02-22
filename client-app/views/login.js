88864bbdd2caf2bde065207157b48e05c44da0c6
Joe Longstreet <joelongstreet@gmail.com>
2014-02-21 23:28:53 -0600

    add form submission handler

--- client-app/views/login.js
+++ client-app/views/login.js
@@ -2,6 +2,16 @@ module.exports = Marionette.ItemView.extend({
 
   className: 'login',
   template: templates.login,
+  events: {
+    'submit form' : 'submit'
+  },
+
+  submit: function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    var userId = $(this.el).find('input').val();
+    $(this.el).removeClass('show');
+  },
 
   onRender: function(){
     var self = this;
