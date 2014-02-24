6389eeedbe71be6d738bb550a8340b0ac2db405d
Joe Longstreet <joelongstreet@gmail.com>
2014-02-24 09:08:51 -0600

    super messy server side control flow

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 var express = require('express');
 var http = require('http');
 var routes = require('./lib/routes');
+var Messenger = require('./lib/messenger');
 var app = express();
+
 
 app.use(express.static(process.cwd() + '/public'));
 
+
 app.get('/*', routes.ui);
 app.get('/api/user/authorize', routes.getAuthURL);
-app.get('/api/auth_callback', routes.authCallback);
+app.get('/api/flickr/auth_callback', routes.authCallback);
 app.get('/api/:method', routes.makeFlickrRequest);
+
 
 var server = http.createServer(app).listen(3000, function(){
   console.log('Express server listening on port 3000');
 });
 
+
 var io = require('socket.io').listen(server);
+io.set('log level', 1);
 io.sockets.on('connection', function(socket){
-  console.log('sockets connected');
+  var messenger = new Messenger(socket);
+  socket.on('login', messenger.login);
+  socket.on('getPhotoSets', messenger.getPhotoSets);
+  socket.on('getPhotoSet', messenger.getPhotoSet);
 });
