7c0a4ca2b7febdc66ae812531e76a042d463bc0c
Joe Longstreet <joelongstreet@gmail.com>
2014-02-24 22:12:59 -0600

    mange user on client

--- client-app/socket.js
+++ client-app/socket.js
@@ -1,5 +1,6 @@
 var socket = io.connect('http://localhost');
 var mediator = require('./mediator');
+var userId = {};
 
 
 var message = function(data){
@@ -15,14 +16,21 @@ var loginFailure = function(){
 };
 
 
-var loginSuccess = function(){
+var loginSuccess = function(data){
   var Message = require('./views/message');
   var model = new Backbone.Model({
     type: 'success',
     text: 'Successfully logged in, fetching photo set'
   });
+  userId = data.user.id;
 
   mediator.trigger('viewUpdate', new Message({ model : model }));
+  socket.emit('flickr.photosets.getList', { user_id : userId });
+};
+
+
+var photoSetList = function(data){
+  console.log(data);
 };
 
 
@@ -30,6 +38,7 @@ var loginSuccess = function(){
 socket.on('login:failure', loginFailure);
 socket.on('login:success', loginSuccess);
 socket.on('message', message);
+socket.on('flickr.photosets.getList', photoSetList);
 
 
 module.exports = socket;
