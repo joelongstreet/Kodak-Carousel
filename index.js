4841b2236bdcf5bf663fc2266a03465d55e403d6
Joe Longstreet <joelongstreet@gmail.com>
2014-02-24 22:12:19 -0600

    rewrite messenger

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require('express');
 var http = require('http');
-var routes = require('./lib/routes');
+var path = require('path');
+var express = require('express');
 var Messenger = require('./lib/messenger');
 var app = express();
 
 
 app.use(express.static(process.cwd() + '/public'));
 
-
-app.get('/*', routes.ui);
-app.get('/api/flickr/auth_callback', routes.authCallback);
-
+app.get('/*', function(req, res){
+  var htmlPath = path.join(process.cwd(), 'public/app.html');
+  res.sendfile(htmlPath);
+});
 
 var server = http.createServer(app).listen(3000, function(){
   console.log('Express server listening on port 3000');
@@ -21,7 +21,4 @@ var io = require('socket.io').listen(server);
 io.set('log level', 1);
 io.sockets.on('connection', function(socket){
   var messenger = new Messenger(socket);
-  socket.on('login', messenger.login);
-  socket.on('getPhotoSets', messenger.getPhotoSets);
-  socket.on('getPhotoSet', messenger.getPhotoSet);
 });
